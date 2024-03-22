0a6f1892fbab997c963d000cf060776c60e5d58e
Preethi <preethieswar8@gmail.com>
2024-03-22 10:44:02 +0530

    updated code

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 
